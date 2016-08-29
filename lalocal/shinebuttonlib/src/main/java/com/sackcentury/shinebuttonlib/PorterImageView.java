6f031df0d30b1439d45f52708546e8b5bede9036
xiaojw <xiaojiwei@mddtrip.cn>
2016-08-29 19:53:54 +0800

    reset

--- lalocal/shinebuttonlib/src/main/java/com/sackcentury/shinebuttonlib/PorterImageView.java
+++ lalocal/shinebuttonlib/src/main/java/com/sackcentury/shinebuttonlib/PorterImageView.java
@@ -57,8 +57,7 @@ public abstract class PorterImageView extends ImageView {
 
     public void setSrcColor(int color){
         paintColor = color;
-//        setImageDrawable(new ColorDrawable(color));
-        setBackground(new ColorDrawable(color));
+        setImageDrawable(new ColorDrawable(color));
         if(drawablePaint!=null){
             drawablePaint.setColor(color);
             invalidate();
