a48a565c53e6c43d300faf90dac681f3d5a72d6b
wangchangjian@mddtrip.cn <wangchangjian@mddtrip.cn>
2016-10-21 13:49:49 +0800

    分享平台提前判断，无则不显示图标

--- lalocal/app/src/main/java/com/lalocal/lalocal/activity/fragment/NewsFragment.java
+++ lalocal/app/src/main/java/com/lalocal/lalocal/activity/fragment/NewsFragment.java
@@ -521,8 +521,7 @@ public class NewsFragment extends BaseFragment implements View.OnClickListener {
             view.requestLayout();
         }
     }
-
-
+    
     private int liveUserId;
     private String createNickName;
     String createAnn = null;
