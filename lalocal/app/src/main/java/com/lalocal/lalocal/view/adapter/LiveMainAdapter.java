da334ef396355056774d52fa7b321bf65cee0aa7
iNerdStack <wjnovember@qq.com>
2017-01-04 17:36:07 +0800

    我的关注页面，正在直播UI修改

--- lalocal/app/src/main/java/com/lalocal/lalocal/view/adapter/LiveMainAdapter.java
+++ lalocal/app/src/main/java/com/lalocal/lalocal/view/adapter/LiveMainAdapter.java
@@ -1,14 +1,12 @@
 package com.lalocal.lalocal.view.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,13 +16,21 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
 import com.lalocal.lalocal.R;
+import com.lalocal.lalocal.live.entertainment.activity.AudienceActivity;
 import com.lalocal.lalocal.live.entertainment.activity.LiveHomePageActivity;
+import com.lalocal.lalocal.live.entertainment.activity.PlayBackActivity;
+import com.lalocal.lalocal.model.Constants;
 import com.lalocal.lalocal.model.LiveRowsBean;
 import com.lalocal.lalocal.model.LiveUserBean;
 import com.lalocal.lalocal.util.AppLog;
+import com.lalocal.lalocal.util.CommonUtil;
 import com.lalocal.lalocal.util.DrawableUtils;
+import com.lalocal.lalocal.util.SPCUtils;
+import com.lalocal.lalocal.view.ScaleImageView;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 import java.util.List;
 
@@ -100,7 +106,7 @@ public class LiveMainAdapter extends RecyclerView.Adapter implements View.OnClic
                 holder = new LiveHightHolder(hightView);
             }
         } else {
-            View view = inflater.inflate(R.layout.live_list_item_new_layout, parent, false);
+            View view = inflater.inflate(R.layout.fragment_live_item, parent, false);
             holder = new LiveViewHodler(view);
             view.setOnClickListener(this);
         }
@@ -135,7 +141,7 @@ public class LiveMainAdapter extends RecyclerView.Adapter implements View.OnClic
                         hightHolder.avatarImg.setOnClickListener(this);
                         hightHolder.nicknameTv.setOnClickListener(this);
                     }
-                    String addres=liveRowsBean.getAddress();
+                    String addres = liveRowsBean.getAddress();
                     hightHolder.locTv.setText(addres);
                     hightHolder.titleTv.setText(liveRowsBean.getTitle());
                     hightHolder.onlineNumTv.setText(String.valueOf(liveRowsBean.getOnlineNumber()));
@@ -155,33 +161,105 @@ public class LiveMainAdapter extends RecyclerView.Adapter implements View.OnClic
                 }
             } else {
                 LiveViewHodler liveViewHodler = (LiveViewHodler) holder;
-                boolean challengeStatus = liveRowsBean.isChallengeStatus();
-                if (liveRowsBean.getStartAt() != null && liveRowsBean.getEndAt() != null) {
-                    liveViewHodler.liveListItemStatus.setText("回放");
-                    liveViewHodler.liveListItemStatus.setBackgroundResource(R.drawable.live_status_playback_bg);
-                } else if (challengeStatus) {
-                    liveViewHodler.liveListItemStatus.setText("挑战任务中");
-                    liveViewHodler.liveListItemStatus.setBackgroundResource(R.drawable.live_status_challenge_bg);
-                } else {
-                    liveViewHodler.liveListItemStatus.setText("直播中");
-                    liveViewHodler.liveListItemStatus.setBackgroundResource(R.drawable.live_status_living_bg);
-                }
-                liveViewHodler.itemLiveName.setText(liveRowsBean.getUser().getNickName());
-                if (liveRowsBean.getTitle() == null) {
-                    liveViewHodler.liveListItemTitle.setText(liveRowsBean.getUser().getNickName());
-                } else {
-                    liveViewHodler.liveListItemTitle.setText(liveRowsBean.getTitle());
+
+                if (liveRowsBean == null) {
+                    return;
                 }
 
-                liveViewHodler.itemLiveAdress.setText(liveRowsBean.getAddress());
-                DrawableUtils.displayImg(mContext, liveViewHodler.liveCompereHeadPortrait, liveRowsBean.getUser().getAvatar());
-                DrawableUtils.displayImg(mContext, liveViewHodler.itemLiveCoverIv, liveRowsBean.getPhoto());
-                liveViewHodler.itemLayout.setOnClickListener(new View.OnClickListener() {
+                liveViewHodler.imgLiveType.setImageResource(R.drawable.homelist_living_ic);
+
+                liveViewHodler.layoutLiveContainer.setFocusable(false);
+                String title = liveRowsBean.getTitle();
+                String address = liveRowsBean.getAddress();
+                String photo = liveRowsBean.getPhoto();
+                String lastMsg = liveRowsBean.getLastMsg();
+                final int targetType = liveRowsBean.getTargetType();
+                final int id = liveRowsBean.getId();
+
+                LiveUserBean user = liveRowsBean.getUser();
+
+                String avatar = null;
+                String nickname = null;
+
+                if (user != null) {
+                    avatar = user.getAvatar();
+                    nickname = user.getNickName();
+                }
+
+                if (TextUtils.isEmpty(title)) {
+                    title = "木有标题哦~";
+                }
+                liveViewHodler.tvTitle.setText(title);
+
+                if (TextUtils.isEmpty(address)) {
+                    address = "地点找不到啦";
+                }
+                liveViewHodler.tvAddress.setText(address);
+
+                if (!TextUtils.isEmpty(photo)) {
+                    Glide.with(mContext)
+                            .load(photo)
+                            .placeholder(R.drawable.androidloading)
+                            .into(liveViewHodler.imgPhoto);
+                }
+
+                if (!TextUtils.isEmpty(lastMsg)) {
+                    liveViewHodler.tvLastMsg.setVisibility(View.VISIBLE);
+                    liveViewHodler.tvLastMsg.setText(lastMsg);
+                } else {
+                    liveViewHodler.tvLastMsg.setVisibility(View.GONE);
+                }
+
+                if (!TextUtils.isEmpty(avatar)) {
+                    DrawableUtils.displayImg(mContext, liveViewHodler.imgAvatar, avatar, R.drawable.androidloading);
+                }
+
+                if (TextUtils.isEmpty(nickname)) {
+                    nickname = "一位不愿意透露...";
+                }
+                liveViewHodler.tvNickname.setText(nickname);
+
+                // 直播回放标签设置
+                if (targetType == Constants.TARGET_TYPE_CHANNEL) {
+                    if (liveRowsBean.isChallengeStatus()) {
+                        liveViewHodler.tvType.setBackgroundResource(R.drawable.bg_tag_challenge);
+                        liveViewHodler.tvType.setText("任务挑战中");
+                    } else {
+                        liveViewHodler.tvType.setBackgroundResource(R.drawable.bg_tag_living);
+                        liveViewHodler.tvType.setText("直播中");
+                    }
+                } else if (targetType == Constants.PLAY_BACK_TYPE_URL) {
+                    liveViewHodler.tvType.setBackgroundResource(R.drawable.bg_tag_playback);
+                    liveViewHodler.tvType.setText("回放");
+                } else if (targetType == 0) {
+
+                    if (liveRowsBean.getEndAt() != null && liveRowsBean.getStartAt() != null) {
+                        liveViewHodler.tvType.setBackgroundResource(R.drawable.bg_tag_playback);
+                        liveViewHodler.tvType.setText("回放");
+                    } else if (liveRowsBean.isChallengeStatus()) {
+                        liveViewHodler.tvType.setBackgroundResource(R.drawable.bg_tag_challenge);
+                        liveViewHodler.tvType.setText("任务挑战中");
+                    } else {
+                        int roomId = liveRowsBean.getRoomId();
+                        liveViewHodler.tvType.setBackgroundResource(R.drawable.bg_tag_living);
+                        liveViewHodler.tvType.setText("直播中");
+                    }
+                }
+
+                liveViewHodler.layoutClick.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
-                        if (onLiveItemClickListener != null) {
-                            onLiveItemClickListener.goLiveRoom(liveRowsBean);
+                        // 我的关注对回放还是直播进行判断
+                        if (targetType == Constants.TARGET_TYPE_CHANNEL) {
+                            AppLog.i("sfr", "1");
+                            Intent intent1 = new Intent(mContext, AudienceActivity.class);
+                            intent1.putExtra("id", String.valueOf(id));
+                            ((Activity) mContext).startActivityForResult(intent1, 123);
+                        } else if (targetType == Constants.PLAY_BACK_TYPE_URL) {
+                            AppLog.i("sfr", "2");
+                            Intent intent = new Intent(mContext, PlayBackActivity.class);
+                            intent.putExtra("id", String.valueOf(id));
+                            ((Activity) mContext).startActivityForResult(intent, 123);
                         }
                     }
                 });
@@ -218,20 +296,29 @@ public class LiveMainAdapter extends RecyclerView.Adapter implements View.OnClic
 
     public class LiveViewHodler extends RecyclerView.ViewHolder {
 
-        @BindView(R.id.live_list_item_title)
-        TextView liveListItemTitle;
-        @BindView(R.id.live_list_item_status)
-        TextView liveListItemStatus;
-        @BindView(R.id.item_live_cover_iv)
-        ImageView itemLiveCoverIv;
-        @BindView(R.id.live_compere_head_portrait)
-        CircleImageView liveCompereHeadPortrait;
-        @BindView(R.id.item_live_name)
-        TextView itemLiveName;
-        @BindView(R.id.item_live_adress)
-        TextView itemLiveAdress;
-        @BindView(R.id.item_layout)
-        LinearLayout itemLayout;
+
+        @BindView(R.id.tv_type)
+        TextView tvType;
+        @BindView(R.id.img_live_type)
+        ImageView imgLiveType;
+        @BindView(R.id.tv_title)
+        TextView tvTitle;
+        @BindView(R.id.icon_my_concern_address)
+        ImageView iconMyConcernAddress;
+        @BindView(R.id.tv_address)
+        TextView tvAddress;
+        @BindView(R.id.tv_nickname)
+        TextView tvNickname;
+        @BindView(R.id.img_avatar)
+        RoundedImageView imgAvatar;
+        @BindView(R.id.img_photo)
+        ScaleImageView imgPhoto;
+        @BindView(R.id.tv_last_msg)
+        TextView tvLastMsg;
+        @BindView(R.id.layout_click)
+        LinearLayout layoutClick;
+        @BindView(R.id.layout_live_container)
+        LinearLayout layoutLiveContainer;
 
         public LiveViewHodler(View itemView) {
             super(itemView);
